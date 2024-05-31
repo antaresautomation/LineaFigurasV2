ac8a21166029720503adf8bce972f8fd8f9f4ebc
agust2001 <agustin.cane2001@gmail.com>
2024-05-31 14:07:28 -0600

    Update Program.cs

--- LineaFigurasV2/Program.cs
+++ LineaFigurasV2/Program.cs
@@ -23,7 +23,7 @@ namespace LineaFigurasV2
         {
             JuguetesConsola JuguetesConsola = new JuguetesConsola();
 
-            // Obtiene el catálogo de datos
+            // Obtiene el catalogo de datos, che fer
             List<Modelos.Figura> figuras = db.Figura.ToList();
             List<Modelos.Color> colores = db.Color.ToList();
 
@@ -32,21 +32,26 @@ namespace LineaFigurasV2
                 Console.WriteLine("------- Seleccionar Modo ------");
                 Console.WriteLine("----- 1. Cancelar Figuras -----");
                 Console.WriteLine("----- 2. Crear una Figura -----");
-                int opcionModo = Convert.ToInt32(Console.ReadLine());
+                string opcionuser = Console.ReadLine();
+                int.TryParse(opcionuser, out int opcionModo);
+
+
                 Console.Clear();
 
-                switch (opcionModo)
+                if (opcionModo == 2)
                 {
-                    case 1:
-                        CancelarFiguras();
-                        break;
-                    case 2:
-                        CrearFigura(figuras, colores);
-                        break;
-                    case 3:
-                        return;
-                    default:
-                        Console.WriteLine("Opción no válida.");
+                    Console.WriteLine("Seleccione el tipo de figura a crear:");
+
+                    // Mostrar opciones de figuras
+                    foreach (Modelos.Figura f in figuras)
+                    {
+                        Console.WriteLine($"|{f.ID}| {f.Figura1}");
+                    }
+
+                    int opcionFigura = Convert.ToInt32(Console.ReadLine());
+                    if (!figuras.Any(x => x.ID == opcionFigura))
+                    {
+                        Console.WriteLine("Opción inexistente");
                         Console.ReadKey();
                         Console.Clear();
                         continue;
@@ -93,19 +98,21 @@ namespace LineaFigurasV2
                     Console.ReadKey();
                     Console.Clear();
                 }
+
                 else if (opcionModo == 1)
                 {
                     List<Item> items = ItemController.ObtenerFilaDeEstacion(1);
                     ItemController.ListaFiguras(items);
                     int idEspecifico = ItemController.InputVerifier(items);
 
-                    var item = db.Item.SingleOrDefault(i => i.ID == idEspecifico);
+                    Item item = items[idEspecifico];
+                    //db.Item.SingleOrDefault(i => i.ID == idEspecifico);
 
                     if (item != null)
                     {
                         ItemController.Cancelar(item);
                         db.SaveChanges(); // Asegúrate de guardar los cambios en la base de datos
-                        Console.WriteLine($"El item con ID {idEspecifico} ha sido cancelado.");
+                        Console.WriteLine($"El item con ID {item.ID} ha sido cancelado.");
                     }
                     else
                     {
@@ -124,78 +131,6 @@ namespace LineaFigurasV2
             }
         }
 
-        static void CancelarFiguras()
-        {
-            List<Item> items = ItemController.ObtenerFilaDeEstacion(1);
-            ItemController.ListaFiguras(items);
-            int idEspecifico = ItemController.InputVerifier(items);
-
-            Item item = items[idEspecifico];
-
-            if (item != null)
-            {
-                ItemController.Cancelar(item);
-                db.SaveChanges(); // Asegúrate de guardar los cambios en la base de datos
-                Console.WriteLine($"El item con ID {item.ID} ha sido cancelado.");
-            }
-            else
-            {
-                Console.WriteLine("Item no encontrado.");
-            }
-            Console.ReadKey();
-            Console.Clear();
-        }
-
-        static void CrearFigura(List<Modelos.Figura> figuras, List<Modelos.Color> colores)
-        {
-            Console.WriteLine("Seleccione el tipo de figura a crear:");
-            foreach (Modelos.Figura f in figuras)
-            {
-                Console.WriteLine($"|{f.ID}| {f.Figura1}");
-            }
-
-            int opcionFigura = Convert.ToInt32(Console.ReadLine());
-            if (!figuras.Any(x => x.ID == opcionFigura))
-            {
-                Console.WriteLine("Opción inexistente");
-                Console.ReadKey();
-                Console.Clear();
-                return;
-            }
-
-            Console.WriteLine("Ingrese el color:");
-            foreach (Modelos.Color c in colores)
-            {
-                Console.WriteLine($"|{c.ID}| {c.Color1}");
-            }
-
-            int opcionColor = Convert.ToInt32(Console.ReadLine());
-            if (!colores.Any(x => x.ID == opcionColor))
-            {
-                Console.WriteLine("Opción inexistente");
-                Console.ReadKey();
-                Console.Clear();
-                return;
-            }
-
-            var nuevoItem = new Modelos.Item
-            {
-                ID_Figura = opcionFigura,
-                ID_Color = opcionColor,
-                ID_Estado = 0,
-                IsActive = true,
-                Origin_Date = DateTime.UtcNow,
-                Edit_Date = DateTime.UtcNow
-            };
-
-            db.Item.Add(nuevoItem);
-            db.SaveChanges();
-
-            int nuevoItemId = nuevoItem.ID;
-            Console.WriteLine($"Item creado con ID: {nuevoItemId}");
-            Console.ReadKey();
-            Console.Clear();
-        }
 
         public class JuguetesConsola
         {
@@ -231,4 +166,5 @@ namespace LineaFigurasV2
             }
         }
     }
+
 }
